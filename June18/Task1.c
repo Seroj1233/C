d958efd10f0345589a0cfd3beaebe2f983414179
Seroj1233 <israelyansh@gmail.com>
2025-07-09 15:02:03 +0400

    Update Task1.c

--- June18/Task1.c
+++ June18/Task1.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
 
+int main(){
+
+	printf("Please input lowercase letter: ");
+	char letter = 0;
+	scanf("%c" , &letter);
+	letter = letter - ('a' - 'A'); 
+	printf("The result is: %c\n" , letter);
+	return 0;
+}
