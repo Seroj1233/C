8bd791b45c5ff715b05d113c6934ef7a115eca05
Seroj1233 <israelyansh@gmail.com>
2025-07-09 14:36:40 +0400

    Update Task1.c

--- June16/Task1.c
+++ June16/Task1.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
 
+int main(){
+    
+	int a = 0;
+	printf("Input the number to get it on the screen: ");
+	scanf ("%d" , &a);
+	printf ("The result is %d\n" , a);
+	return 0;
+	
+}
