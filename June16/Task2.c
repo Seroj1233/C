9ea34f02a207e2f3e199029657b09fa23ba5985f
Seroj1233 <israelyansh@gmail.com>
2025-07-09 14:39:27 +0400

    Update Task2.c

--- June16/Task2.c
+++ June16/Task2.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+int main(){
+    
+  	double a = 0;
+  	printf("Input the number to get it on the screen: ");
+  	scanf("%lf" , &a);
+    printf("The result is %lf" , a);
+ 	return 0;
+    
+	
+}
