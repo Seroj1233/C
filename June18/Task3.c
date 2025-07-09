bd71ccf811cb63db063618989bd8860c728a41dd
Seroj1233 <israelyansh@gmail.com>
2025-07-09 15:02:59 +0400

    Update Task3.c

--- June18/Task3.c
+++ June18/Task3.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(){
+
+	int x = 0;
+	int y = 0;
+	printf("Input the first number ");
+	scanf("%d" , &x);
+	printf("Input the second number ");
+	scanf("%d" , &y);
+	int res = x + y;
+	printf("The result is %d\n" , res);
+	return 0;
+}
