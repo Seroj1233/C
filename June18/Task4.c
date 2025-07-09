3b470339d0c73749d41151585a00b1cdb0d7d316
Seroj1233 <israelyansh@gmail.com>
2025-07-09 15:03:23 +0400

    Update Task4.c

--- June18/Task4.c
+++ June18/Task4.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(){
+
+	int length = 0;
+	int width = 0;
+	printf("Enter the length ");
+	scanf("%d" , &length);
+	printf("Enter the width ");
+	scanf("%d" , &width);
+	int Area = length * width;
+	printf("The result of Area is %d\n" , Area);
+	return 0;
+}
