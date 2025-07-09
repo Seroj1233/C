22a3ae0280165d2a3963bc60740e7047b93aacc6
Seroj1233 <israelyansh@gmail.com>
2025-07-09 14:43:52 +0400

    Update Task4.c

--- June16/Task4.c
+++ June16/Task4.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main(){
-    
-    int x = 0;
-    scanf("%d" , &x);
+	
+	int x = 0;
+   	scanf("%d" , &x);
 	printf("The result is %d" , (4 * x + 21 * x * x -12));
 	return 0; 
 
