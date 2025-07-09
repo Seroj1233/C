711e515ee8afd3241198d1a9611d1c31cfe3e68a
Seroj1233 <israelyansh@gmail.com>
2025-07-09 14:42:54 +0400

    Update Task7.c

--- June16/Task7.c
+++ June16/Task7.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+int main(){
+    
+  float x = 0;
+	float y = 0;
+	printf("Input the first number ");
+	scanf("%f" , &x);
+	printf("Input the second number ");
+	scanf("%f" , &y);
+	printf("The result is %f \n x = %f \n y = %f" , (x + y) / 2 , x , y);
+	return 0;
+
+	
+}
