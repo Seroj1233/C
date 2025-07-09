290c014d668fd41f206cc9513ed70f806f49da97
Seroj1233 <israelyansh@gmail.com>
2025-07-09 15:03:43 +0400

    Update Task5.c

--- June18/Task5.c
+++ June18/Task5.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
 
+int main(){
+
+	double Celsius = 0;
+	printf("Enter the Celsius ");
+	scanf("%lf" , &Celsius);
+	printf("The result of Celsius in Farenheit %lf \n" , (Celsius * 9/5) + 32);
+	return 0;
+}
