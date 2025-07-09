faa9b6738b1417680cd0121d5139e3731c0005f2
Seroj1233 <israelyansh@gmail.com>
2025-07-09 15:02:32 +0400

    Update Task2.c

--- June18/Task2.c
+++ June18/Task2.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
 
+int main(){
+
+	char letter = 0;
+	printf("Input uppercase letter to get lowercase %c\n" , letter);
+	scanf("%c" , &letter);
+	letter = letter + ('a' - 'A');
+	printf("The result is %c\n" , letter);
+	return 0;
+}
