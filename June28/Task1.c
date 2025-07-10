9d96f6c179a551bf4bc819c9eb7af4b465d0c3c7
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:53:40 +0400

    Update Task1.c

--- June28/Task1.c
+++ June28/Task1.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+int main(){
+    
+    int x = 0;
+    printf("Input the number to reverse it: ");
+    scanf("%d" , &x);
+    x = ~x + 1;
+    printf("The result is: %d" , x);
+    return 0;
+    
+}
