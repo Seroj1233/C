1b1d041c14d16dafb257b79e64faa78203b5598b
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:54:06 +0400

    Update Task3.c

--- June28/Task3.c
+++ June28/Task3.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+int main(){
+    
+    int x = 34;
+    int n = 0;
+    printf("Input the n-th bit to set it: ");
+    scanf("%d" , &n);
+    x = x | 1 << n;
+    printf("The result is: %d" , x);
+    return 0;
+    
+}
