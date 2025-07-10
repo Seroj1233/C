ebf34345b2f07b8c53b3b4c2328f9ad2c1c0a4fd
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:48:38 +0400

    Update Task7.c

--- June21/Task7.c
+++ June21/Task7.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
 
+int main(){
+    
+    printf("Size of int: %ld " , sizeof(int));
+    printf("Size of char: %ld " , sizeof(char));
+    printf("Size of float: %ld " , sizeof(float));
+    printf("Size of double: %ld " , sizeof(double));
+    return 0;
+}
