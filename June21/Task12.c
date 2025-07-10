bf5faa75016496b97b1b9aa3704ff870e15c78f5
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:49:47 +0400

    Update Task12.c

--- June21/Task12.c
+++ June21/Task12.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+int main(){
+    
+    double a = 0;
+    printf("Enter the first number: ");
+    scanf("%lf" , &a);
+    double b = 0;
+    printf("Enter the second number: ");
+    scanf("%lf" , &b);
+    double c = 0;
+    printf("Enter the third number: ");
+    scanf("%lf" , &c);
+    double res = 0;
+    res = (a + b + c) / 3;
+    printf("The average of three is: %lf" , res);
+    return 0;
+    
+}
