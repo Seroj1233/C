985194c566f140515007984f400cc80adbc70377
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:54:45 +0400

    Update Task6.c

--- June28/Task6.c
+++ June28/Task6.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+int main(){
+    
+    int x = 0;
+    int n = 2;
+    printf("Input the number to multiply by 2: ");
+    scanf("%d" , &x);
+    x = ( x << 1);
+    printf("The result is: %d" , x);
+    return 0;
+    
+}
