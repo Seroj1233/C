99a32025bc41093fe17e52f30f55214c16840148
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:49:31 +0400

    Update Task11.c

--- June21/Task11.c
+++ June21/Task11.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#define PI 3.14
 
+int main(){
+    
+    double r = 0;
+    printf("Enter The Radius: ");
+    scanf("%lf" , &r);
+    double res = 0;
+    res = PI * r * r;
+    printf("The result is: %f" , res);
+    return 0;
+}
