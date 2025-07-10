609979105c7e812b2b9ec04083b93b13f8f92ed4
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:54:33 +0400

    Update Task5.c

--- June28/Task5.c
+++ June28/Task5.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+int main(){
+    
+    int x = 34;
+    int n = 0;
+    printf("Input the n-th bit to toggle: ");
+    scanf("%d" , &n);
+    x = x ^ ( 1 << n);
+    printf("The result is: %d" , x);
+    return 0;
+    
+}
