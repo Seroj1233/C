73936654cf44b9373bc66d7145893f9239a7ee3e
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:48:51 +0400

    Update Task8.c

--- June21/Task8.c
+++ June21/Task8.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+int main(){
+    
+    int a = 0;
+    int b = 0;
+    printf("Enter the first number: ");
+    scanf("%d" , &a);
+    printf("Enter the second number: ");
+    scanf("%d" , &b);
+    int res = 0;
+    res = a % b;
+    printf("The result is: %d" , res);
+    return 0;
+}
