8e5436157f91915299010cb6140b1cfc4148451e
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:49:04 +0400

    Update Task9.c

--- June21/Task9.c
+++ June21/Task9.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
 
+int main(){
+    
+    int a = 0;
+    printf("Enter The number to get its cube: ");
+    scanf("%d" , &a);
+    int res = 0;
+    res = a * a * a;
+    printf("The cube is: %d" , res);
+    return 0;
+}
