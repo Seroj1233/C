25aee9e87e844c36d6909d7f37fdfd0db6132b48
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:48:25 +0400

    Update Task6.c

--- June21/Task6.c
+++ June21/Task6.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
 
+int main(){
+    
+    printf("Enter any character: ");
+    char character = 0;
+    scanf("%c" , &character);
+    printf("Your ASCII value is: %d" , character);
+    return 0;
+}
