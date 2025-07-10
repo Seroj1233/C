b2b1412cefa61c51d29aa79c2e557a979e2c270b
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:53:55 +0400

    Update Task2.c

--- June28/Task2.c
+++ June28/Task2.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+int main(){
+    
+    int x = 0;
+    int n = 2;
+    printf("Input the number to get the power of that number: ");
+    scanf("%d" , &n);
+    x = 1 << n;
+    printf("The result is: %d" , x);
+    return 0;
+    
+}
