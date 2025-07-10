e5aac002e05c552e563599e0fe169e5b6ab49618
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:55:09 +0400

    Update Task8.c

--- June28/Task8.c
+++ June28/Task8.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main(){
+    
+    int x = 0;
+    int y = 0;
+    printf("Input the first number to swap: x = ");
+    scanf("%d" , &x);
+    printf("Input the second number to swap: y = ");
+    scanf("%d" , &y);
+    x = x ^ y;
+    y = x ^ y;
+    x = y ^ x;
+    printf("The result is: x = %d y = %d" , x , y);
+    return 0;
+    
+    
+}
