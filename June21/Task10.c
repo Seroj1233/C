be0e18f310af886b8f4e52c60d801ce43bb03d03
Seroj1233 <israelyansh@gmail.com>
2025-07-10 23:49:19 +0400

    Update Task10.c

--- June21/Task10.c
+++ June21/Task10.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main(){
+    
+    int a = 0;
+    int b = 0;
+    printf("Enter the first number to swap:a =  ");
+    scanf("%d" , &a);
+    printf("Enter the second number to swap:b =  ");
+    scanf("%d" , &b);
+    printf("Your Entered numbers: a = %d, b = %d\n", a , b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("The result is: a = %d , b =%d " , a , b);
+    return 0;
+    
+}
