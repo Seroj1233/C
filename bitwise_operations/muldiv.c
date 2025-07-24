a4227eeb55cffefc54c43618fe3bdd4924ccb27a
Sergey <israelyansh@gmail.com>
2025-07-25 00:17:31 +0400

    bitwise_operations

--- bitwise_operations/muldiv.c
+++ bitwise_operations/muldiv.c
@@ -4,12 +4,17 @@ int main(){
 
 	int num = 0;
 	int n = 0;
+	int mul;
+	int div;
 	printf("Enter the num to multiply then divide: ");
 	scanf("%d" , &num);
 	printf("Enter the power: ");
 	scanf("%d" , &n);
-	if ( 1 << n && n >> 1){
-		printf("The result is: %d\n");
+	if ( n >= 0){
+		mul = num << n;
+		div = num >> n;
+		        printf("The result of multiplication: %d , %d" , mul ,div);
+     //   printf("Divided by 2^%d = %d\n", n, divided);
 	}
 	return 0;
 
